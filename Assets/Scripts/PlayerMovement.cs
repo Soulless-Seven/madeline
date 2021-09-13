87791affeb114c7c220972669c98db68a0582523
Justin R <u1134889@umail.utah.edu>
2021-09-13 15:03:35 -0600

    Adds customizable first person movement to player with child camera

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody _rigidbody;
+    private Camera _camera;
+    private Vector2 _currRotation;
 
-    public float ForwardVelocity;
-    public float BackwardVelocity;
-    public float LateralVelocity;
-    public float VerticalSensitivity;
-    public float HorizontalSensitivity;
+    public float ForwardVelocity = 3f;
+    public float BackwardVelocity = 1f;
+    public float LateralVelocity = 2f;
+    public float VerticalSensitivity = 10f;
+    public float HorizontalSensitivity = 10f;
+    public float MaximumYAngle = 80f;
 
     public float horiz;
     public float vert;
@@ -19,19 +22,29 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         _rigidbody = gameObject.GetComponent<Rigidbody>();
+        _camera = gameObject.GetComponentInChildren<Camera>();
+        _currRotation = new Vector2(0f, 0f);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        _currRotation.x += Input.GetAxis("Mouse X") * HorizontalSensitivity;
+        _currRotation.y -= Input.GetAxis("Mouse Y") * VerticalSensitivity;
+        _currRotation.x = Mathf.Repeat(_currRotation.x, 360);
+        _currRotation.y = Mathf.Clamp(_currRotation.y, -MaximumYAngle, MaximumYAngle);
+        transform.rotation = Quaternion.Euler(0, _currRotation.x, 0);
+        _camera.transform.localRotation = Quaternion.Euler(_currRotation.y, 0, 0);
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         float transZ = Input.GetAxis("Vertical");
-        vert = transZ; // TODO: removeme
         float transX = Input.GetAxis("Horizontal");
-        horiz = transX; // TODO: removeme
-        float rotX = Input.GetAxis("Mouse Y");
-        float rotY = Input.GetAxis("Mouse X");
 
         float vZ = transZ > 0 ? transZ * ForwardVelocity : transZ * BackwardVelocity;
-        _rigidbody.velocity = new Vector3(transX * LateralVelocity, 0, vZ);
+        _rigidbody.velocity = Quaternion.Euler(0, _currRotation.x, 0) * new Vector3(transX * LateralVelocity, 0, vZ);
     }
 }
