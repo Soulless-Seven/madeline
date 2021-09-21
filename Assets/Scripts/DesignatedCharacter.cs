d462423b7962e05e80fbf6c029027b6d2e75f4e3
Alcatrax-x <bazbate@gmail.com>
2021-09-21 16:17:59 -0600

    Fixed Character Swapping, it works now!
    InputManager must have a key called "Swap_Character", I put it as Q.
    Swaps character view and control when pressed
    Character 1 is default.
    
    --Must have a CharacterManager Empty GameObject to work!--

--- Assets/Scripts/DesignatedCharacter.cs
+++ Assets/Scripts/DesignatedCharacter.cs
@@ -2,48 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 //@author Sebastian Bate
-//First time Q is pressed, choose the first character
-//afterwards it swaps character control
-//
-//How to use:
-//Drag onto 1st character, and drag onto 2nd character
-//fill in the "Other Character" field by dragging the other character into it
+/**
+ * Switches Character Control and Camera View
+ * 
+ * Requires a CharacterManager with the Designated Character Script attached 
+ * Q and left control must be Character switch and crouch respectively
+ * 
+ * How to use:
+ * Drag Cameras into their respective slots
+ * Drag Characters into their respective slots
+ * 
+ */
 
 
 public class DesignatedCharacter : MonoBehaviour
 {
-    //public GameObject camera1;
-    //public GameObject camera2;
 
-    public GameObject otherCharacter;
+    public GameObject camera1;
+    public GameObject camera2;
+
+    //public GameObject otherCharacter;
+    public GameObject character1;
+    public GameObject character2;
+
+
+    /**
+     * Character 1 is set as default
+     * Therefore Camera1 and Controls are enabled by default only for character 1
+     */
     void Start ()
-        {
-            //camera1.setActive(true);
-            //camera2.setActive(false);
-        }
+    {
+        camera1.SetActive(true);
+        camera2.SetActive(false);
+        character1.GetComponent<PlayerMovement>().enabled = true;
+        character2.GetComponent<PlayerMovement>().enabled = false;
+        character1.GetComponent<CrouchScript>().enabled = true;
+        character2.GetComponent<CrouchScript>().enabled = false;
+    }
     void Update()
     {
-        if (Input.GetButtonDown("Swap_Character"))
+        //swap from character 1 to character 2
+        if (Input.GetButtonDown("Swap_Character") && character1.GetComponent<PlayerMovement>().enabled == true)
         {
-            //disable 2nd character, control 1st character
-            //inverse each time Q is pressed
-            otherCharacter.GetComponent<PlayerMovement>().enabled = false;
-            GetComponent<PlayerMovement>().enabled = true;
-            //switch camera
-           // if (camera1.setActive(true))
-           // {
-               // camera1.setActive(true);
-             //   camera2.setActive(false);
-            //}
-           // else
-            //{
-            //    camera1.setActive(true);
-            //    camera2.setActive(false);
-           // }
-            //same for crouch script
-            //idk if it's needed, stfu
-            otherCharacter.GetComponent<CrouchScript>().enabled = false;
-            GetComponent<CrouchScript>().enabled = true;
+            //disable movement
+            character1.GetComponent<PlayerMovement>().enabled = false;
+            //GetComponent<PlayerMovement>().enabled = true;
+            character2.GetComponent<PlayerMovement>().enabled = true;
+            //disable camera
+            camera1.SetActive(false);
+            camera2.SetActive(true);
+            //disable crouch script
+            character1.GetComponent<CrouchScript>().enabled = false;
+            character2.GetComponent<CrouchScript>().enabled = true;
+
+        }
+        else if (Input.GetButtonDown("Swap_Character") && character2.GetComponent<PlayerMovement>().enabled == true)
+        {
+            character1.GetComponent<PlayerMovement>().enabled = true;
+            character2.GetComponent<PlayerMovement>().enabled = false;
+
+            camera1.SetActive(true);
+            camera2.SetActive(false);
+
+            character1.GetComponent<CrouchScript>().enabled = true;
+            character2.GetComponent<CrouchScript>().enabled = false;
         }
     }
 }
